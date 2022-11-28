025c8a3a309b7b61ca88d385eb553263c2dfa8d8
Mike Wall <daikazu@gmail.com>
2022-11-28 00:32:29 -0500

    wip

--- src/GA4ServiceProvider.php
+++ src/GA4ServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace Daikazu\GA4;
 
+use Daikazu\GA4\Events\BroadcastEvent;
+use Daikazu\GA4\Events\EventBroadcaster;
 use Daikazu\GA4\Http\ClientIdRepository;
 use Daikazu\GA4\Http\ClientIdSession;
+use Daikazu\GA4\Listeners\DispatchAnalyticsJob;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Event;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -29,6 +34,8 @@ class GA4ServiceProvider extends PackageServiceProvider
             return new GA4();
         });
 
+        $this->app->singleton(EventBroadcaster::class, BroadcastEvent::class);
+
         $this->app->singleton(ClientIdRepository::class, ClientIdSession::class);
 
         $this->app->bind('ga4-event-tracking.client-id', function () {
@@ -45,7 +52,7 @@ class GA4ServiceProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
-        Event::listen(ShouldBroadcastToAnalytics::class, DispatchAnalyticsJob::class);
+//        Event::listen(ShouldBroadcastToAnalytics::class, DispatchAnalyticsJob::class);
 
         Blade::directive('sendClientID', function () {
             return "<?php echo view('ga4-event-tracking::sendClientID'); ?>";
